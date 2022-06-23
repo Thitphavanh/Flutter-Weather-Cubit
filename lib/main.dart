a440b4b965c9add233ad38b7a51e8e8d9a1c6b67
Hery <thitphavanh.psv@gmail.com>
2022-06-23 16:58:59 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_weather_cubit/cubits/weather/weather_cubit.dart';
+import 'package:flutter_weather_cubit/repositories/weather_repository.dart';
+import 'package:flutter_weather_cubit/services/weather_api_services.dart';
+
+import 'pages/home_page.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(
+        weatherApiServices: WeatherApiServices(
+          httpClient: http.Client(),
+        ),
+      ),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<WeatherCubit>(
+            create: (context) => WeatherCubit(
+              weatherRepository: context.read<WeatherRepository>(),
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Weather App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomePage(),
+        ),
+      ),
+    );
+  }
+}
